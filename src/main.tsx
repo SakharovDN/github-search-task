cb4994fb53226cd89ce1bf85f9d29196499da32c
SakharovDN <deafsah@gmail.com>
2025-06-25 02:33:11 +0700

    feat(app): renamed app folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router';
 
-import { App } from './app2/App';
+import { App } from './app/App';
 
 import './app/styles/main.scss';
 
