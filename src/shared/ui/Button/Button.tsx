7bff2bfb92a42d5e316acaf969b908cfbf556f56
SakharovDN <deafsah@gmail.com>
2025-06-25 20:24:05 +0700

    feat(Button): add Button component

---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,57 @@
+import {
+  ButtonHTMLAttributes,
+  cloneElement,
+  DetailedHTMLProps,
+  JSXElementConstructor,
+  ReactElement,
+  SVGProps,
+} from 'react';
+
+import clsx from 'clsx';
+
+import { Loader } from '../Loader/Loader';
+
+import styles from './Button.module.scss';
+
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  block?: boolean;
+  loading?: boolean;
+  iconPosition?: IconPosition;
+  appearance?: ButtonAppearance;
+  icon?: ReactElement<SVGProps<SVGSVGElement>, JSXElementConstructor<SVGProps<SVGSVGElement>> | string>;
+}
+
+type ButtonAppearance = 'accent' | 'primary';
+type IconPosition = 'end' | 'start';
+
+export const Button = ({
+  appearance = 'primary',
+  iconPosition = 'start',
+  className,
+  children,
+  disabled,
+  loading,
+  block,
+  icon,
+  ...props
+}: ButtonProps) => {
+  const buttonStyles = clsx(
+    styles.button,
+    styles[appearance],
+    block && styles.block,
+    loading && styles.loading,
+    disabled && styles.disabled,
+    iconPosition === 'end' && styles.rtl,
+    className
+  );
+
+  const iconClone = icon ? cloneElement(icon, { className: styles.icon }) : null;
+
+  return (
+    <button className={buttonStyles} disabled={disabled || loading} {...props}>
+      {loading && <Loader centered />}
+      {iconClone}
+      {children && <span className={styles.text}>{children}</span>}
+    </button>
+  );
+};
