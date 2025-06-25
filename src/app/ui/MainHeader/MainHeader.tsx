02ac9e529bb0babf5c5340b5c79f960bfb48b6cf
SakharovDN <deafsah@gmail.com>
2025-06-25 16:06:19 +0700

    feat(MainHeader): add search icon in main header

--- src/app/ui/MainHeader/MainHeader.tsx
+++ src/app/ui/MainHeader/MainHeader.tsx
@@ -1,9 +1,12 @@
+import { SearchIcon } from '@/shared/ui/icons';
+
 import styles from './MainHeader.module.scss';
 
 export const MainHeader = () => {
   return (
     <header className={styles.mainHeader}>
-      <div>
+      <div className={styles.titleContainer}>
+        <SearchIcon className={styles.searchIcon} />
         <p className={styles.title}>GitHubSearch</p>
       </div>
     </header>
