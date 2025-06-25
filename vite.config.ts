f422fad18674224106122677944163adcfc744dd
SakharovDN <deafsah@gmail.com>
2025-06-25 16:05:45 +0700

    feat: installed vite-plugin-svgr

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/github-search-task/',
-  plugins: [react(), checker({ typescript: true }), tsconfigPaths()],
+  plugins: [react(), checker({ typescript: true }), tsconfigPaths(), svgr()],
   server: { open: true },
   css: {
     modules: {
