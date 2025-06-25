1cccfafb405e4c64363cade89dcf0b0ac719a4fd
SakharovDN <deafsah@gmail.com>
2025-06-25 19:18:04 +0700

    feat(Icons): add icons GitBranch and Star

--- src/shared/ui/icons/index.ts
+++ src/shared/ui/icons/index.ts
@@ -1,1 +1,3 @@
+export { default as GitBranchIcon } from './gitBranch.svg?react';
 export { default as SearchIcon } from './search.svg?react';
+export { default as StarIcon } from './star.svg?react';
