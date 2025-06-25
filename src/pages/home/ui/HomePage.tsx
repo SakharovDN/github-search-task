4579594ac10daed6787475bdcb29d89546cc4120
SakharovDN <deafsah@gmail.com>
2025-06-25 18:16:12 +0700

    feat(HomePage): implement search functionality of repositories

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,9 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import { useDebounce } from '@/shared/lib/timing';
+import { Input } from '@/shared/ui/Input';
 import { Page } from '@/shared/ui/Page';
 
+import { RepositoriesList, repositoryStore } from '@/entities/repository';
+
 export const HomePage = () => {
+  const [search, setSearch] = useState('');
+  const debouncedSearch = useDebounce(search);
+
+  useEffect(() => {
+    repositoryStore.searchRepositories(debouncedSearch);
+  }, [debouncedSearch]);
+
   return (
     <Page title="Главная">
-      <h1>HomePage</h1>
+      <Input placeholder="Search" type="search" value={search} onChange={(e) => setSearch(e.target.value)} />
+
+      <RepositoriesList />
     </Page>
   );
 };
